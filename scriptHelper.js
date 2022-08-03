d4978115610b35c123db60c393daed3bc54db0ea
Greenlee412 <ryanjgreenlee@gmail.com>
2022-08-02 22:09:59 -0500

    2nd revision

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,7 +2,7 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
+//    Here is the HTML formatting for our mission target div.
    /*
                 <h2>Mission Destination</h2>
                 <ol>
@@ -14,80 +14,97 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+
+    let destination = document.getElementById("missionTarget")
+    destination.innerHTML = `
+    <h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter} </li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons}</li>
+                </ol>
+                    <img src="${imageUrl}">
+                    `
 }
 
 function validateInput(testInput) {
-        window.addEventListener("load", function() {
-            let form = document.querySelector("form")
-            form.addEventListener("submit", function() {
-                let pilotInput = document.querySelector("input[name=pilotName]");
-                let copilotInput = document.querySelector("input[name=copilotName]");
-                let fuelInput = document.querySelector("input[name=fuelLevel]");
-                let cargoInput = document.querySelector("input[name=cargoMass]");
-
-                if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || cargoInput.value === "") {
-                    alert("Entry in all fields is required")
-                    event.preventDefault()
-                } else if (isNaN(pilotInput.value) === false || isNaN(copilotInput.value) === false) {
-                    alert("Please enter a valid name for the Pilot and/or Co-Pilot.")
-                    event.preventDefault()
-                } else if (isNaN(fuelInput.value) || isNaN(cargoInput.value)) {
-                    alert("Please enter a valid number for Fuel Level and/or Cargo Mass.");
-                    event.preventDefault()
-                } else {
-                    // This alert is to confirm that all fields have the correct typeof value submitted. Can remove once page is complete
-                    alert("All required information successfully submitted!")
-                    document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotInput.value} is ready.`
-                    document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilotInput.value} is ready.`
-                    
-                    // Need to write conditional for fuel levels here, including changing the DOM elements and style
-                    if(fuelInput.value < 10000) {
-                        document.getElementById("faultyItems").style.visibility = "visible"
-                        document.getElementById("fuelStatus").innerHTML = "Not enough fuel for launch."
-                        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
-                        document.getElementById("launchStatus").style.color = "red"
-                        
-                    } else {
-                        document.getElementById("fuelStatus").innerHTML = "Shuttle has enough fuel for launch."
-                    }
-                    
-                    if(cargoInput.value > 10000) {
-                        document.getElementById("faultyItems").style.visibility = "visible"
-                        document.getElementById("cargoStatus").innerHTML = "Too much mass for takeoff."
-                        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
-                        document.getElementById("launchStatus").style.color = "red"
-                    } else {
-                        document.getElementById("cargoStatus").innerHTML = "Cargo mass within acceptable range for launch."
-                    }
-                    // Write conditional here to confirm shuttle is ready for launch given correct fuelLevel and cargoMass values. Should update the document text and color
-                    if (fuelInput.value >= 10000 && cargoInput.value < 10000) {
-                        document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch"
-                        document.getElementById("launchStatus").style.color = "green"
-                    }
-                    //Need event.preventDefault() here to stop the browser window from reloading on submission
-                    event.preventDefault();
-                }
+            let test;
+            
+                if (testInput === "") {
+                    return "Empty";
+            } else if (!isNaN(testInput)) {
+              
+                return "Is a Number";
+            } else if (isNaN(testInput)) {
                 
-            })
-        })
-    };
-    
+                return "Not a Number";
+            }
+        };
+           
+  
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-        // I have absolutely no idea what should go here or how to separate code from the validateInput function if it contains too much.
-    
+                     
+                let vPilotInput = validateInput(pilot.value);
+                let vCopilotInput = validateInput(copilot.value);
+                let vFuelInput = validateInput(fuelLevel.value);
+                let vCargoInput = validateInput(cargoLevel.value);
+                
+             
+                if (vPilotInput === "Empty" || vCopilotInput === "Empty" || vFuelInput === "Empty" || vCargoInput === "Empty") {
+                    alert("Entry in all fields is required")
+                
+                    
+                } else if (vPilotInput === "Is a Number" || vCargoInput === "Is a Number" || vFuelInput === "Not a Number" || vCargoInput === "Not a Number") {
+                    alert("Please enter a valid name for the Pilot and/or Co-Pilot.")
+                    
+                } else {
    
+    this.document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot.value} is ready.`
+    this.document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilot.value} is ready.`
+                }
+    
+    if(fuelLevel.value < 10000) {
+        this.document.getElementById("faultyItems").style.visibility = "visible"
+        this.document.getElementById("fuelStatus").innerHTML = "Not enough fuel for launch."
+        this.document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+        this.document.getElementById("launchStatus").style.color = "red"
+        
+    } else {
+        this.document.getElementById("fuelStatus").innerHTML = "Shuttle has enough fuel for launch."
+    }
+    
+    if(cargoLevel.value > 10000) {
+        this.document.getElementById("faultyItems").style.visibility = "visible"
+        this.document.getElementById("cargoStatus").innerHTML = "Too much mass for takeoff."
+        this.document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+        this.document.getElementById("launchStatus").style.color = "red"
+    } else {
+        this.document.getElementById("cargoStatus").innerHTML = "Cargo mass within acceptable range for launch."
+    }
+    // Write conditional here to confirm shuttle is ready for launch given correct fuelLevel and cargoMass values. Should update the this.document text and color
+    if (fuelLevel.value >= 10000 && cargoLevel.value < 10000) {
+        this.document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch"
+        this.document.getElementById("launchStatus").style.color = "green"
+   
+}
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) { 
+        return response.json()
+})
+        return planetsReturned
+};
 
-    return planetsReturned;
-}
+
 
 function pickPlanet(planets) {
+   let index = Math.floor(Math.random() * planets.length)
+      return index
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
